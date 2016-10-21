454a9a2450bb64f56508f7a8caf6126cc901ecc7
Anant4830 <anant4830@gmail.com>
2016-10-22 00:33:45 +0530

    Add files via upload

---
+++ java_session2Assingment1/src/java_session2Assingment1/printname.java
@@ -0,0 +1,13 @@
+package java_session2Assingment1;
+
+public class printname {
+
+	public static void main(String[] args) {
+		if(System.out.printf("ANANT") == null){
+		
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
